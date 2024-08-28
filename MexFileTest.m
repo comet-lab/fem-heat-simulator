230ae6cac22c5f3bf007bec231d3d5794f73db3d
Nicholas Pacheco <nepacheco@wpi.edu>
2024-08-28 14:05:45 -0400

    changes to mex testing

--- MexFileTest.m
+++ MexFileTest.m
@@ -1,6 +1,6 @@
 clear MEX_Heat_Simulation
 tissueSize = [1.0,1.0,0.5];
-nodeSize = [35,35,101];
+nodeSize = [35,35,51];
 ambientTemp = 20;
 T0 = single(ambientTemp*ones(nodeSize));
 deltaT = 0.05;
@@ -14,7 +14,7 @@ HTC = 0.075;
 useAllCPUs = true;
 silentMode = false;
 Nn1d = 2;
-layerInfo = [tissueSize(3)/10,50];
+layerInfo = [0.025,30];
 sensorPositions = [0,0,0];
 
 w = @(z) w0 * sqrt(1 + (z.*10.6e-4./(pi*w0.^2)).^2);
@@ -31,16 +31,14 @@ BC = int32(2*ones(6,1)); %0: HeatSink, 1: Flux, 2: Convection
 Jn = 0;
 %% 
 tic
-[TpredLayer,sensorTemps] = MEX_Heat_Simulation(T0,NFRLayer,tissueSize',tFinal,...
+[TpredLayer,sensorTempsLayer] = MEX_Heat_Simulation(T0,NFRLayer,tissueSize',tFinal,...
     deltaT,tissueProperties,BC,Jn,ambientTemp,sensorPositions,useAllCPUs,...
     silentMode,layerInfo);
-sensorTemp = sensorTemps(:,end);
-fullSensor = sensorTemps;
 toc
 %%
-nodeSize = [35,35,301];
+nodeSize = [35,35,601];
 T0 = single(ambientTemp*ones(nodeSize));
-layerInfo = [tissueSize(3),101];
+layerInfo = [tissueSize(3),nodeSize(3)-1];
 x = linspace(-tissueSize(1)/2,tissueSize(1)/2,nodeSize(1));
 y = linspace(-tissueSize(2)/2,tissueSize(2)/2,nodeSize(2));
 z = linspace(0,tissueSize(3),nodeSize(3));
@@ -50,26 +48,41 @@ tic
 [Tpred,sensorTemps] = MEX_Heat_Simulation(T0,NFR,tissueSize',tFinal,...
     deltaT,tissueProperties,BC,Jn,ambientTemp,sensorPositions,useAllCPUs,...
     silentMode,layerInfo);
-sensorTemp = sensorTemps(:,end);
-fullSensor = sensorTemps;
 toc
 
 %%
 figure(1);
 clf;
-plot(0:deltaT:tFinal,sensorTemps,'LineWidth',2);
+hold on;
+plot(0:deltaT:tFinal,sensorTempsLayer,'LineWidth',2,'DisplayName',"Two Layers");
+plot(0:deltaT:tFinal,sensorTemps,'LineWidth',2,'DisplayName',"One Layer");
+hold off
 grid on;
 xlabel("Time (s)");
 ylabel("Temperature (deg C)");
 title("Sensor temperature over time");
+legend()
 
 figure(2);
 clf;
+tiledlayout('flow');
+nexttile()
 hold on
-plot(zLayer,reshape(NFRLayer(17,17,:),size(z)),'LineWidth',2)
-plot(z,reshape(NFR(17,17,:),size(z)),'LineWidth',2)
+plot(zLayer,reshape(NFRLayer(17,17,:),size(zLayer)),'LineWidth',2,'DisplayName',"Two Layers")
+plot(z,reshape(NFR(17,17,:),size(z)),'LineWidth',2,'DisplayName',"One Layers")
 hold off
 grid on;
+legend()
+xlabel("Penetration depth (cm)");
+ylabel("Normalized Fluence Rate");
+title("Normalized Fluence Rate with 2 Layers");
+nexttile()
+hold on
+plot(zLayer(1:30),reshape(NFRLayer(17,17,1:30),size(zLayer(1:30))),'LineWidth',2,'DisplayName',"Two Layers")
+plot(z(1:30),reshape(NFR(17,17,1:30),size(z(1:30))),'LineWidth',2,'DisplayName',"One Layers")
+hold off
+grid on;
+legend()
 xlabel("Penetration depth (cm)");
 ylabel("Normalized Fluence Rate");
 title("Normalized Fluence Rate with 2 Layers");
@@ -77,7 +90,7 @@ title("Normalized Fluence Rate with 2 Layers");
 figure(3);
 clf;
 hold on
-plot(zLayer,reshape(TpredLayer(17,17,:),size(z)),'LineWidth',2,'DisplayName',sprintf("Two Layers"))
+plot(zLayer,reshape(TpredLayer(17,17,:),size(zLayer)),'LineWidth',2,'DisplayName',sprintf("Two Layers"))
 plot(z,reshape(Tpred(17,17,:),size(z)),'LineWidth',2,'DisplayName',sprintf("One Layer"))
 hold off
 grid on;
@@ -85,7 +98,31 @@ xlabel("Depth (cm)");
 ylabel("Temperautre (deg C)");
 title("Temperature Prediction with 2 layers");
 legend()
+%%
+figure(4)
+clf;
+tiledlayout('flow')
+nexttile()
+surf(X(:,:,1),Y(:,:,1),TpredLayer(:,:,1))
+xlabel("X Axis (cm)")
+ylabel("Y Axis (cm)");
+title("Suraface Temperature Plot Two-layer mesh")
+c = colorbar();
+c.Label.String = 'Temperature (deg C)';
+axis equal
+view(0,90);
+colormap('hot')
 
 
+nexttile()
+surf(X(:,:,1),Y(:,:,1),Tpred(:,:,1))
+xlabel("X Axis (cm)")
+ylabel("Y Axis (cm)");
+title("Suraface Temperature Plot One-layer mesh")
+c = colorbar();
+c.Label.String = 'Temperature (deg C)';
+axis equal
+view(0,90);
+colormap('hot')
 % h_f2 = plotVolumetric.plotVolumetric(2,x,y,z,Tpred,'MCmatlab_fromZero');
 % title(sprintf('Cpp Final Temp, [C]'))
