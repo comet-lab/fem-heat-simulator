5d767f5b0605dec86aa3662680643e818a3a559c
Nicholas Pacheco <nepacheco@wpi.edu>
2025-09-21 15:44:11 -0400

    compileMex also installs and clears modules

--- HelperScripts/compileMex.sh
+++ HelperScripts/compileMex.sh
@@ -11,3 +11,10 @@ rm -rf build
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DUSE_CUDA=ON -DAMGX_ROOT=/home/nepacheco/amgx/ ..
 cmake --build .
+cmake --install .
+
+module unload gcc/12.1.0
+module unload cmake
+module unload eigen
+module unload matlab
+module unload cuda/12.6.3
